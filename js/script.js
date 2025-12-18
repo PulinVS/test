1ac2815a2beca7045406fd77e0a527ca081240c5
Vadim <vadimpulin1488@gmail.com>
2025-12-18 23:58:36 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+'use strict';
+const a = 4 + '5';
+console.log(typeof(a));
+console.log(a);
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+console.log(++incr);
+console.log(2*4 == 8);
+
+const b = true,
+      c = false;
+console.log(b && !c);     
